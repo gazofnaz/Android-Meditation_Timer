0efd9a9f7135a972d80a16015ddefc0b7dd1300d
gazofnaz <gazofnaz31@hotmail.com>
2013-10-26 15:16:05 +0100

    Hard code 15 minute timer
    Service is playing sound correctly

--- src/main/java/com/gazmx/meditationtimer/MeditationService.java
+++ src/main/java/com/gazmx/meditationtimer/MeditationService.java
@@ -2,6 +2,7 @@ package com.gazmx.meditationtimer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import android.widget.Toast;
  */
 public class MeditationService extends Service {
     private MyCount counter;
+    private MediaPlayer mpAudio;
 
     @Override
     public IBinder onBind(Intent arg0){
@@ -21,7 +23,7 @@ public class MeditationService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId ){
         //this service will run until we stop it
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
-        counter = new MyCount(5000,1000);
+        counter = new MyCount(900000,1000);
         counter.start();
         return START_STICKY;
     }
@@ -42,8 +44,9 @@ public class MeditationService extends Service {
         //after the count is up, we start the song
         @Override
         public void onFinish() {
+            mpAudio = MediaPlayer.create(MeditationService.this, R.raw.singingbowl);
+            mpAudio.start();
             stopService(new Intent(getBaseContext(), MeditationService.class));
-            //mpAudio.start();
             //final TextView textViewToChange = (TextView) findViewById(R.id.t_duration);
             //textViewToChange.setText("Done!");
         }
